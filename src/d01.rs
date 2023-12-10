1a349b5e4c6960316e0f3eb000462d03f4dc65c7
IlReSenzaNome <alexisvivas3@gmail.com>
2023-12-09 19:35:46 -0500

    First commit

---
+++ src/d01.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let r = 2;
+    println!("The area of the circle is {}", area(r));
+}
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    const MAX_POINTS: u32 = 100_000;
+    print!("Hello, world! {}", MAX_POINTS);
+
+    let x = 5;
+    println!("Hello, world! {}", x);
+    let x = "a";
+    println!("Hello, world! {}", x);
+}
